a25430d9a9baf45aa96b733fd5e3ea4f42de4b69
abrizalsyahputra93-alt <abrizalsyahputra93@gmail.com>
2025-11-12 19:45:21 +0700

    Update auth.ts

--- api/auth.ts
+++ api/auth.ts
@@ -1,7 +1,7 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 const CLIENT_ID = "01K9SEDGKEAB4KH7WAJ8Q2XGVD";
-const REDIRECT_URI = "https://kbv-2tq3.vercel.app/api/callback"; // ganti sesuai domain vercel kamu
+const REDIRECT_URI = "https://kbv-2tq3.vercel.app/api/callback"; // ganti sesuai project Vercel kamu
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const authUrl = `https://kick.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
@@ -9,4 +9,3 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   )}&response_type=code&scope=chat:write`;
   res.redirect(authUrl);
 }
-
